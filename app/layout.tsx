6c67ab21f5309e4598748e1ca0d62bb0e516a0ad
Braimah-Abiola <braimahabiola.official@gmail.com>
2024-05-08 10:16:59 +0000

    First commit

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+
+import { ModalProvider } from "@/providers/modal-provider";
+import { ToastProvider } from "@/providers/toast-provider";
+import { ThemeProvider } from "@/providers/theme-provider";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Acme - Dashboard",
+  description: "Acme admin dashboard",
+};
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider
+      appearance={{
+        variables: {
+          colorPrimary: "#18181B",
+        },
+      }}
+    >
+      <html lang="en">
+        <body className={inter.className}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <ToastProvider />
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
