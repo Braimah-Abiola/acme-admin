d592b7437f379edd75c110a81074cf66bfb54531
Braimah-Abiola <braimahabiola.official@gmail.com>
2024-05-27 13:32:14 +0000

    Color update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,16 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 
 import { ModalProvider } from "@/providers/modal-provider";
-import { ToastProvider } from "@/providers/toast-provider";
 import { ThemeProvider } from "@/providers/theme-provider";
+import { ToastProvider } from "@/providers/toast-provider";
 
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Acme - Dashboard",
-  description: "Acme admin dashboard",
+  title: "Dripends - Dashboard",
+  description: "Dripends admin dashboard",
 };
 
 export default async function RootLayout({
@@ -23,7 +23,7 @@ export default async function RootLayout({
     <ClerkProvider
       appearance={{
         variables: {
-          colorPrimary: "#18181B",
+          colorPrimary: "#123296",
         },
       }}
     >
